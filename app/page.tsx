8e01c3eebc0429ee1b43bac7696bf408ee5a6a6e
SANTHOSHDHANANJEYAN <santhoshd318@gmail.com>
2025-08-20 11:44:10 +0530

    7713354

--- app/page.tsx
+++ app/page.tsx
@@ -28,8 +28,6 @@ export default function Home() {
           <AmenitiesPreview />
           <ContactPreview/>
           <SocialNetworksSection />
-          
-          <LocationPreview />
         </Suspense>
       </main>
       <Footer />
