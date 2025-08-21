61797161f368e399228461692ba11fc3a38e839a
SANTHOSHDHANANJEYAN <santhoshd318@gmail.com>
2025-08-21 23:35:23 +0530

    7713354

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -88,7 +88,7 @@ const Footer = () => {
         </div>
 
         <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 Asteya South Cliff Beach View Villas. All rights reserved.</p>
+          <p>&copy; 2025 - Asteya beach villa - All rights reserved</p>
         </div>
       </div>
     </footer>
