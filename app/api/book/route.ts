5e769c6066ccf86ac8889c20e7736f57f4f2971c
santhoshkumar <kumarsanthosh2743@gmail.com>
2025-08-23 14:48:43 +0530

    18-08

--- app/api/book/route.ts
+++ app/api/book/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import connectDB from "@/lib/mongodb";
+import { connectDB } from "@/lib/mongodb";  
 import Booking from "@/models/Booking";
 import { sendOwnerNotification, sendGuestConfirmation } from "@/lib/email";
 
