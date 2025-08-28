de9182b831d208b06f2cffde9fb1cbbe53c658e8
SANTHOSHDHANANJEYAN <116083112+SANTHOSHDHANANJEYAN@users.noreply.github.com>
2025-08-28 19:16:06 +0530

    Update page.tsx

--- app/villas/[id]/page.tsx
+++ app/villas/[id]/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useState } from "react";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/layout/Navbar";
 
 // Your villa type
 interface Villa {
