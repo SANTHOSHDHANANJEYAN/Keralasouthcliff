01861a5bd1df972f2e27539847a316780ab572db
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-08-30 17:13:00 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,14 @@
 import ContactSection from '@/components/sections/ContactSection';
+import LocationPreview from '@/components/sections/LocationPreview';
+import SocialNetworksSection from '@/components/sections/SocialNetworksSection';
 
 export default function ContactPage() {
   return (
     <div className="min-h-screen">
       <main className="pt-20">
         <ContactSection />
+        <SocialNetworksSection/>
+        <LocationPreview />
       </main>
     </div>
   );
