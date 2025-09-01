fc2ee46722b1cbce1ae7f6a1d108404f03f95b3a
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-09-01 23:37:04 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,6 +2,7 @@ import ContactSection from '@/components/sections/ContactSection';
 import LocationPreview from '@/components/sections/LocationPreview';
 import SocialNetworksSection from '@/components/sections/SocialNetworksSection';
 import Footer from "@/components/layout/Footer";
+import WhatsAppButton from '@/components/ui/WhatsAppButton';
 
 export default function ContactPage() {
   return (
@@ -13,5 +14,8 @@ export default function ContactPage() {
       </main>
       <Footer />
     </div>
+    <div className="fixed bottom-6 right-6 z-50">
+        <WhatsAppButton />
+      </div>
   );
 }
